fdc3b4dbd5f2f4001ce324c97f55e4b100850290
AugustoMoro <morovaldebuia@gmail.com>
2016-10-08 18:12:54 -0300

    Commit1

---
+++ src/mtreversivel/Fita.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mtreversivel;
+
+/**
+ *
+ * @author Augusto
+ */
+public class Fita {
+    private int TAMFITA;
+    private int posicao;
+    private char[] fita;
+    public Fita(){
+        
+    }
+}
